3a574286a060242738125a1eac5dce51d9300f14
Yatnesh1410 <yattugyani14@gmail.com>
2022-11-08 20:03:51 +0530

    first commit

---
+++ src/components/Download.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import assets from '../assets';
+import styles from '../styles/Global';
+
+const Download = () => {
+  return (
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>Get the full source code on GitHub</p>
+        </div>
+        <button className={styles.btnPrimary} onClick={() => window.open("https://github.com/Yatnesh1410/React_Native_Mobile_App", "_blank")}>
+          Source Code
+        </button>
+        <div className={styles.flexCenter}>
+          <img  src={assets.scene} alt="scene" className={styles.fullImg} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Download
